474aac98584c7efcd8f1f3a20037331f0ad9a427
Alexandre1111111111 <127530540+Alexandre1111111111@users.noreply.github.com>
2023-06-01 22:15:27 -0300

    Curso

--- Banco.sql
+++ Banco.sql
@@ -1,1 +1,10 @@
 -- Active: 1685574299112@@127.0.0.1@3306@sakila
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Alexandre11122007Alews';
+create database DBsql;
+use DBsql;
+create table contas(
+codigo int(4) auto_increment,
+email varchar(50) not null,
+senha varchar(30) not null,
+primary key (codigo)
+);
