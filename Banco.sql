eabb21be3eaaac2a858a5d60e1951cc569b26d71
Alexandre1111111111 <127530540+Alexandre1111111111@users.noreply.github.com>
2023-06-01 23:22:08 -0300

    Curso

--- Banco.sql
+++ Banco.sql
@@ -3,7 +3,7 @@ ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Alexandr
 create database DBsql;
 use DBsql;
 create table contas(
-codigo int(4) auto_increment,
+codigo int(6) auto_increment,
 email varchar(50) not null,
 senha varchar(30) not null,
 primary key (codigo)
