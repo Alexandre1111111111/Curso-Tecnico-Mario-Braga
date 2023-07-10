284095fe64246acd1d519cc52027a46f6ec7e80b
Alexandre1111111111 <127530540+Alexandre1111111111@users.noreply.github.com>
2023-07-10 18:34:14 -0300

    Curso

--- Banco.sql
+++
@@ -1,9 +0,0 @@
--- Active: 1686703178162@@127.0.0.1@3306@dbsql
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Alexandre11122007Alews';
-create database if not exists DBsql;
-use DBsql;
-create table if not exists contas(
-id_codigo smallint auto_increment primary key,
-email varchar(50) not null,
-senha varchar(20) not null 
-);
--- protect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!isset($_SESSION)) {
-    session_start();
-}
-if (!isset($_SESSION['codigo'])) {
-    die("Acesso Negado porque você não está logado.<p><a href=\"index.php\">Entrar</a></p> ");
-}
-?>
