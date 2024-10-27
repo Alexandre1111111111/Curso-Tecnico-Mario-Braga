640c2bce0d1003e800483a61be9d0d77cd01089d
Alexandre <127530540+Alexandre1111111111@users.noreply.github.com>
2024-10-27 18:03:42 -0300

    Add files via upload

--- Escola.js
+++ Escola.js
@@ -1,6 +1,9 @@
 const morse = document.querySelector(".morse");
 const sec = document.querySelector("#sec");
 
+const seg = new Audio("seg.mp3");
+const press = new Audio("press.mp3");
+
 morse.addEventListener("click", () => {
     location.href = "https://static.vecteezy.com/system/resources/previews/017/615/834/non_2x/international-morse-code-illustration-vector.jpg";
 })
@@ -9,11 +12,14 @@ sec.addEventListener("click", () => {
     morse.style.display = "block";
     window.addEventListener("keydown", tecla);
     sec.style.display = "none";
+    seg.play();
 })
 
 let txt = "";
 
 function tecla(e) {
+    press.currentTime = 0;
+    press.play();
     const letra = e.key;
     switch (letra) {
         case "d":
